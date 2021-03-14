ce62ff0c875521994558015a3667fe43fc682b10
redAnt <xuzhijian0107@163.com>
2021-03-14 22:19:53 +0800

    上传插入排序代码

---
+++ InsertionSort/源文件/InsertionSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void insertSort(vector<int>& vec);
+int main() {
+    vector<int> vec;
+    /*
+    for (int i = 0; i < 8; i++) {
+        int n;
+        cin >> n;
+        vec.push_back(n);
+    }
+    */
+    vec.push_back(56);
+    vec.push_back(25);
+    vec.push_back(37);
+    vec.push_back(58);
+    vec.push_back(95);
+    vec.push_back(19);
+    vec.push_back(73);
+    vec.push_back(30);
+    insertSort(vec);
+    for (int k = 0; k < vec.size(); k++) {
+        cout << vec[k] << " ";
+    }
+    return 0;
+}
+void insertSort(vector<int>& vec) {
+    for (int i = 1; i < vec.size(); i++) {
+        int key = vec[i];//记录该位置的值，这个位置之前的数字都已经被排序好
+        //满足条件之后，往后挪动为插入腾出空间 
+        while (i > 0 && key < vec[i - 1]) {
+            //下面两行代码为数据的插入腾出空间
+            vec[i] = vec[i - 1];
+            vec[i - 1] = key;
+            //下标回归到key上，进行下一轮的比对
+            i--;
+        }
+    }
+}
