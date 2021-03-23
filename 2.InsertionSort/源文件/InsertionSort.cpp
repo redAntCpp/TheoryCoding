257e662de2aef96f4b4fdc79bb4501bdca8cceee
redAnt <xuzhijian0107@163.com>
2021-03-23 15:27:00 +0800

    增加逐条输出示例，便于观察排序情况

--- 2.InsertionSort/源文件/InsertionSort.cpp
+++ 2.InsertionSort/源文件/InsertionSort.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 void insertSort(vector<int>& vec);
+//新增函数，输出示例
+string outNum(vector<int> vec);
 int main() {
     vector<int> vec;
-    /*
-    for (int i = 0; i < 8; i++) {
-        int n;
-        cin >> n;
-        vec.push_back(n);
-    }
-    */
     vec.push_back(56);
     vec.push_back(25);
     vec.push_back(37);
@@ -20,14 +16,14 @@ int main() {
     vec.push_back(73);
     vec.push_back(30);
     insertSort(vec);
-    for (int k = 0; k < vec.size(); k++) {
-        cout << vec[k] << " ";
-    }
     return 0;
 }
+//插入排序
 void insertSort(vector<int>& vec) {
+    cout << "开始排序：" << endl;
     for (int i = 1; i < vec.size(); i++) {
         int key = vec[i];//记录该位置的值，这个位置之前的数字都已经被排序好
+        cout << "第" + to_string(i) + "轮：" + outNum(vec) + " 记录值为："+ to_string(key) << endl;
         //满足条件之后，往后挪动为插入腾出空间 
         while (i > 0 && key < vec[i - 1]) {
             //下面两行代码为数据的插入腾出空间
@@ -37,4 +33,14 @@ void insertSort(vector<int>& vec) {
             i--;
         }
     }
+    cout << "排序完毕：" << endl;
+}
+//以字符串形式输出
+string outNum(vector<int> vec) {
+    string output;
+    for (int k = 0; k < vec.size(); k++) {
+        //cout << vec[k] << " ";
+        output = output + to_string(vec[k]) + " ";
+    }
+    return output;
 }
